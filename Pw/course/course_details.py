d3399210eeda8b5073f5de35890b9bb1af804825
mannu chaurasiya <mannuchaurasiya36@gmail.com>
2023-09-30 21:32:01 +0530

    first Commit

---
+++ Pw/course/course_details.py
@@ -0,0 +1,9 @@
+import os, sys
+from os.path import dirname, join , abspath
+sys.path.insert(0, abspath(join(dirname(__file__),'..')))
+from payment import payment_details
+
+def course():
+    print("this is my course details")
+
+payment_details()
---
+++ Pw/payment/payment_details.py
@@ -0,0 +1,2 @@
+def payment():
+    print("this is my payment details")
